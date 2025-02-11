d56349a617dd2906012c5eb8e383da17527a2f44
ayhem8hamdi <ayhemhamdi24@gmail.com>
2025-02-11 14:22:34 +0100

    Preparing dark & light theme part 1

---
+++ e_commerce_app/lib/core/utils/theme/appTheme.dart
@@ -0,0 +1,26 @@
+import 'package:e_commerce_app/constants.dart';
+import 'package:e_commerce_app/core/utils/theme/custom_themes/text_theme_customized.dart';
+import 'package:flutter/material.dart';
+
+abstract class AppTheme {
+  static ThemeData lightTheme() {
+    return ThemeData(
+      useMaterial3: true,
+      fontFamily: 'Poppins',
+      brightness: Brightness.light,
+      primaryColor: kPrimaryColor,
+      scaffoldBackgroundColor: Colors.white,
+      textTheme: TextThemeCustomized.lightTextTheme(),
+    );
+  }
+
+  static ThemeData darkTheme() {
+    return ThemeData(
+        useMaterial3: true,
+        fontFamily: 'Poppins',
+        brightness: Brightness.dark,
+        primaryColor: kPrimaryColor,
+        scaffoldBackgroundColor: Colors.black,
+        textTheme: TextThemeCustomized.lightTextTheme());
+  }
+}
